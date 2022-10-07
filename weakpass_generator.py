d37dc967ecb0287ef375a2821ef349d34fbebab4
nyxgeek <nyxgeek@rslabs.co>
2022-10-07 04:05:02 -0500

    added common password variations- thnx @rootsecdev
    added common 'password' variations

--- weakpass_generator.py
+++ weakpass_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# 2020.02.18 - @nyxgeek - TrustedSec
+# 2020 - @nyxgeek - TrustedSec
 
 # generate weak passwords based on current date
 
@@ -12,8 +12,8 @@ monthDictionary["January"] = ["January", "Winter"]
 monthDictionary["February"] = ["February", "Winter"]
 monthDictionary["March"] = ["March", "Winter", "Spring"]
 monthDictionary["April"] = ["April", "Spring"]
-monthDictionary["May"] = ["May", "Spring", "Summer"]
-monthDictionary["June"] = ["June", "Spring","Summer"]
+monthDictionary["May"] = ["May", "Spring"]
+monthDictionary["June"] = ["June", "Summer"]
 monthDictionary["July"] = ["July", "Summer"]
 monthDictionary["August"] = ["August", "Summer", "Fall", "Autumn"]
 monthDictionary["September"] = ["September", "Fall", "Autumn"]
@@ -21,20 +21,28 @@ monthDictionary["October"] = ["October", "Fall", "Autumn", "Winter"]
 monthDictionary["November"] = ["November", "Fall", "Autumn", "Winter", "Thanksgiving"]
 monthDictionary["December"] = ["December", "Winter", "Christmas"]
 
+standardList = ["password", "Password", "Password1", "Password1!", "Password123", "Password123!", "P@$$w0rd"]
+
 OUTPUT_LIST = []
 
 
 def create_passwords(tempdate):
+    global OUTPUT_LIST
+
+    for password in standardList:
+        #print(password)
+        OUTPUT_LIST.append("%s" % password)
+
+
     year_short=tempdate.strftime("%y")
     year_long=tempdate.strftime("%Y")
     current_month=tempdate.strftime("%B")
 
-    SUFFIX_ARRAY = [ year_short ,  year_long, "@"+year_short, "@"+year_long, year_short+"!", year_long+"!", "1", "123"]
+    SUFFIX_ARRAY = [ year_short ,  year_long, "@"+year_short, "@"+year_short+"!", "@"+year_long, "@"+year_long+"!", year_short+"!", year_long+"!", "1", "123"]
 
     for month_prefix in monthDictionary[current_month]:
         for password_suffix in SUFFIX_ARRAY:
             #print("%s%s" % (month_prefix, password_suffix) )
-            global OUTPUT_LIST
             OUTPUT_LIST.append("%s%s" % (month_prefix, password_suffix))
 
 
@@ -45,6 +53,7 @@ for numberofdays in range(1,90):
 
 #print the unique ones
 print("Here are the results:")
+
 
 OUTPUT_LIST.sort()
 output_set = sorted(set(OUTPUT_LIST))
@@ -59,5 +68,8 @@ for candidate_password in output_set:
     outfile.write(candidate_password+"\n")
 
 
+outfile.write("\n\n@nyxgeek - TrustedSec - 2020.02.20\n")
+
 #close our file now
 outfile.close()
+
